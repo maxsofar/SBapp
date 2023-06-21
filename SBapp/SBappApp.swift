5669662461141776af13be7d9dec97a356cc1669
Max <maxshermant@gmail.com>
2023-06-21 21:45:01 +0300

    Initial Commit

---
+++ SBapp/SBappApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBappApp.swift
+//  SBapp
+//
+//  Created by Max on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SBappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
